1429c7cb9ce6d3c1edab2740220b535a97785ea5
Jesse Liberty <jesse.liberty@cnhind.com>
2022-12-22 09:30:22 -0500

    ForgetMeNotDemo Initial Checkin

---
+++ ForgetMeNotDemo/ForgetMeNotDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ForgetMeNotDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ForgetMeNotDemo/ForgetMeNotDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ForgetMeNotDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
