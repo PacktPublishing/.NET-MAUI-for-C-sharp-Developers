2ecddc9f5a4c7524de36a41a802be89149f1120c
Jesse Liberty <jesse.liberty@cnhind.com>
2023-01-13 16:45:43 -0500

    Persistence and SQLite

---
+++ ForgetMeNotDemo/ForgetMeNotDemo/Database/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace ForgetMeNotDemo.Database;
+
+public static class Constants
+{
+  public const string DatabaseFilename = "ForgetMeNotDemo.db3";
+
+  public const SQLite.SQLiteOpenFlags Flags =
+    SQLite.SQLiteOpenFlags.ReadWrite |
+    SQLite.SQLiteOpenFlags.Create |
+    SQLite.SQLiteOpenFlags.SharedCache;
+
+  public static string DatabasePath =>
+    Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+}
